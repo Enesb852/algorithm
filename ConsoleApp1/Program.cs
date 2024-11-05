462062678e39611383fa2db56decc0582a08578a
Enesb852 <enesbolat852@gmail.com>
2024-11-05 15:37:55 +0300

    girilen değerlerden en küçüğünü bulan program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0, sayac, ek = 0;
+
+            for (sayac = 0; sayac < 5; sayac++)
+            {
+                Console.WriteLine("Bir sayı girin: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayac == 1) // Sayaç 1 olduğunda bunu en küçük değer olarak ayarla
+                {
+                    ek = sayi;
+                }
+
+                if(sayi < ek) { ek = sayi; }
+            }
+
+            Console.WriteLine("En küçük değer: " + ek);
+        }
+    }
+}
+
